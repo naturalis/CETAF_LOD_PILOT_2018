0ea8442c93bcc482e777902a43c96a0a344998e0
ayco-at-naturalis <ayco.holleman@naturalis.nl>
2018-07-08 17:33:39 +0200

    Now storing statements into repository

--- naturalis-lod-etl/src/main/java/nl/naturalis/lod/rdf/gen/RdfGenerator.java
+++ naturalis-lod-etl/src/main/java/nl/naturalis/lod/rdf/gen/RdfGenerator.java
@@ -5,18 +5,18 @@ import java.io.InputStream;
 
 import com.fasterxml.jackson.core.JsonParseException;
 
-import nl.naturalis.lod.util.json.JsonObjectHandler;
 import nl.naturalis.lod.util.json.JsonStreamException;
 import nl.naturalis.lod.util.json.JsonStreamProcessor;
 
 public class RdfGenerator {
 
-  public static void main(String[] args)
-      throws JsonParseException, IOException, JsonStreamException {
-	  SpecimenRdfGenerator srg = new SpecimenRdfGenerator();
-    InputStream is = RdfGenerator.class.getResourceAsStream("/nba-aves-10-specimens.json");
-    JsonStreamProcessor josp = JsonStreamProcessor.create(is, srg);
-    josp.process();
-  }
+	public static void main(String[] args) throws JsonParseException, IOException, JsonStreamException {
+		//SpecimenRdfGenerator srg = new SpecimenRdfGenerator();
+		try (SpecimenRdfLoader loader = new SpecimenRdfLoader()) {
+			InputStream is = RdfGenerator.class.getResourceAsStream("/nba-aves-10-specimens.json");
+			JsonStreamProcessor josp = JsonStreamProcessor.create(is, loader);
+			josp.process();
+		}
+	}
 
 }
